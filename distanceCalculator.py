0ef1c9817e6f802257f772ec0d20a9b985bbf35e
Sri Madur <sri.madur@gmail.com>
2025-01-25 16:13:22 -0500

    basic structure of front-end

--- distanceCalculator.py
+++ distanceCalculator.py
@@ -7,10 +7,20 @@ details = handler.getDetails()
 location = details.loc
 
 latitude = location[0:location.index(",")]
-longitude = location[location.index(",")]
+longitude = location[location.index(",")+1:]
 
-int_latitude = int(latitude)
-int_longitude = int(longitude)
+int_latitude = float(latitude)
+int_longitude = float(longitude)
 
 print(int_latitude + int_longitude)
+
+sites = []
+
+for i in range(4):
+    distance = ((lat[i] - int_latitude) ** 2) + ((long[i] - int_longitude) ** 2)
+    if (distance <= 10):
+        sites.append([name[i], description[i], lat[i], long[i]])
+
+
+
 
