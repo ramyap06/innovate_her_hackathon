a92d3fe059bc17934105fbb6fa61b65860ff6a54
Ramya Prasanna <prasannr@purdue.edu>
2025-01-25 15:08:52 -0500

    Found geolocation

--- UserGeoLocation.py
+++ UserGeoLocation.py
@@ -1,21 +1,8 @@
-import requests
 import googlemaps
 
-# ipinfo_url = "https://ipinfo.io/json?token=4cc93606f30bd1"
-#response = requests.get(ipinfo_url)
-#location_data = response.json()
-
-#user_lat = location_data["loc"].split(",")[0]
-#user_lon = location_data["loc"].split(",")[1]
-
-# Replace with your API Key from Google Cloud
 API_KEY = "AIzaSyCMq3bpKoGR6S3QDRyZqq7Y2szxfebmP4w"
 gmaps = googlemaps.Client(key=API_KEY)
+response = gmaps.geolocate()
 
-# Use Google Maps Geolocation API
-result = gmaps.geolocate()
-
-# Print the location result (Latitude & Longitude)
-print("Your current location:")
-print("Latitude:", result['location']['lat'])
-print("Longitude:", result['location']['lng'])
+user_lat = response['location']['lat']
+user_lon = response['location']['lng']
